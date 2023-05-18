3ec719e4c00076591b98684b283dc778368bb85c
Maddy Ali <maddyoali@gmail.com>
2023-05-18 14:07:09 -0500

    Add server

--- server.js
+++ server.js
@@ -1,9 +1,80 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+const mongodb = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
+let dbConnectionStr = process.env.DB_STRING;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db("fullstack-bank");
+    const questionsCollection = db.collection("questions");
+    console.log(`Connected to ${db.databaseName}`);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    /*
+    ========== Express Handlers ========== 
+    */
+
+    app.set("view engine", "ejs");
+    // Replaces body-parser:
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use("/public", express.static(__dirname + "/public"));
+
+    // READ
+    app.get("/", (req, res) => {
+      db.collection("questions")
+        .find()
+        .toArray()
+        .then((result) => res.render("index.ejs", { questions: result }))
+        .catch((err) => console.log(err));
+    });
+
+    // CREATE
+    app.post("/createQuestion", (req, res) => {
+      questionsCollection
+        .insertOne(req.body)
+        .then((result) => res.redirect("/"))
+        .catch((err) => console.log(err));
+    });
+
+    // UPDATE
+    app.put("/updateQuestion", (req, res) => {
+      try {
+        questionsCollection
+          .updateOne(
+            { _id: new mongodb.ObjectId(req.body.id) },
+            {
+              $set: {
+                question: req.body.question,
+                type: req.body.type,
+              },
+            }
+          )
+          .then((result) => {
+            console.log(result);
+            res.json("Updated Question");
+          });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    // DELETE
+    app.delete("/deleteQuestion", (req, res) => {
+      try {
+        questionsCollection
+          .deleteOne({ _id: new mongodb.ObjectId(req.body.id) })
+          .then((result) => {
+            console.log("Question Deleted");
+            res.json("Question Deleted");
+          });
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
