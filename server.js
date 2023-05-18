ac0c26a92a044539f01bd6be24460fed3148574c
Maddy Ali <maddyoali@gmail.com>
2023-05-18 16:18:58 -0500

    Add port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 const mongodb = require("mongodb");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
